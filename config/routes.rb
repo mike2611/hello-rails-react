b17182a1ea038c392d982801b477788a697d2518
mike2611 <mpuentes2611@gmail.com>
2022-02-15 12:23:37 -0600

    Add redux and configureStore

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { format: 'json'} do
     get 'greeting', to: 'greeting#index'
   end
+
+  get '*page', to: 'static#index', constraints: ->(req) do 
+    !req.xhr? && req.format.html?
+  end
+  
+  root 'static#index'
 end
